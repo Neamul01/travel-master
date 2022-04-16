9bd14c709a3d3b2657757c2c91d7a9abb781ff89
Neamul01 <neamulhaque2002@gmail.com>
2022-04-17 04:31:05 +0600

    header edited

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -40,19 +40,6 @@ const Header = () => {
                             <Link className="block w-1/2 px-3 py-2 mx-1 text-sm font-medium leading-5 text-center text-white transition-colors duration-200 transform bg-gray-500 rounded-md hover:bg-blue-600 md:mx-2 md:w-auto" to="/">Login</Link>
 
                         </div>
-
-                        {/* <!-- Search input on mobile screen --> */}
-                        <div className="mt-3 md:hidden">
-                            <div className="relative">
-                                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                    <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                                        <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
-                                    </svg>
-                                </span>
-
-                                <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search" />
-                            </div>
-                        </div>
                     </div>
                 </div>
 
