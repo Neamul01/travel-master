c2050a1e639d4b1c16ad9858082ca10156c8b6af
Neamul01 <neamulhaque2002@gmail.com>
2022-04-18 02:21:09 +0600

    google and facebook login added

--- src/components/Login/SocialLogin/SocialLogin.js
+++ src/components/Login/SocialLogin/SocialLogin.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GoogleIcon from '@mui/icons-material/Google';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import auth from '../../../firebase.init'
 
 const SocialLogin = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, user, loading, facebookError] = useSignInWithFacebook(auth);
+
+    if (user) {
+        console.log(user)
+    }
+
+    const handleGoogleSignin = () => {
+        signInWithGoogle()
+    }
+
+    const handleFacebookLonin = () => {
+        signInWithFacebook()
+    }
+
     return (
         <div>
             <div className="flex items-center justify-between mt-4">
@@ -14,14 +31,14 @@ const SocialLogin = () => {
             </div>
 
             <div className="flex items-center mt-6 -mx-2">
-                <button type="button"
+                <button onClick={handleGoogleSignin} type="button"
                     className="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
                     <GoogleIcon />
 
                     <span className="hidden mx-2 sm:inline">Sign in with Google</span>
                 </button>
 
-                <button
+                <button onClick={handleFacebookLonin}
                     className="p-1 mx-2 text-sm font-medium text-gray-500 transition-colors duration-200 transform bg-gray-300 rounded-md hover:bg-gray-200">
                     <FacebookIcon className='text-gray-700' style={{ fontSize: '1.8rem' }} />
                 </button>
