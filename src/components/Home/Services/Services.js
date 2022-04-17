d2e0d35b196005ef11d315044933d41fe31aeb4d
Neamul01 <neamulhaque2002@gmail.com>
2022-04-17 18:06:39 +0600

    services updated

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import tiger from '../../../images/one-day.jpg';
+import twoDay from '../../../images/two-day.jpg';
+import week from '../../../images/week.jpg';
 
 const Services = () => {
     return (
@@ -10,59 +13,59 @@ const Services = () => {
                 <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
                         <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                            </svg>
+                            <img className=' rounded-full' src={tiger} alt="" />
                         </span>
 
-                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Copy & paste components</h1>
+                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Day Tour</h1>
 
                         <p className="text-gray-500 dark:text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+                            I can go with you to guide for one day tour .You will get a hole day service and the most atractive places that can cover in a day.Let's Checkout.
                         </p>
 
-                        <a href="#" className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                            <span className="mx-1">read more</span>
-                            <svg className="w-4 h-4 mx-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </a>
+                        <div className='flex justify-between items-center w-full px-4'>
+                            <p className='text-gray-800 font-bold'>Price:</p>
+                            <button className="  -mx-1 text-sm py-2 px-4 rounded-md text-gray-200 bg-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <span className="mx-1">Checkout</span>
+                            </button>
+                        </div>
                     </div>
 
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
                         <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                            </svg>
+                            <img className=' rounded-full' src={twoDay} alt="" />
                         </span>
 
-                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Zero Configrations</h1>
+                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Two Day Tour</h1>
 
                         <p className="text-gray-500 dark:text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+                            I will go with you to guide for a 2 day tour. It will cover some distant travel that cab be cover in two day, with hotel.Lets Check it out.
                         </p>
 
-                        <a href="#" className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                            <span className="mx-1">read more</span>
-                            <svg className="w-4 h-4 mx-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </a>
+                        <div className='flex justify-between items-center w-full px-4'>
+                            <p className='text-gray-800 font-bold'>Price:</p>
+                            <button className="  -mx-1 text-sm py-2 px-4 rounded-md text-gray-200 bg-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <span className="mx-1">Checkout</span>
+                            </button>
+                        </div>
                     </div>
 
                     <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
-                        <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                            </svg>
+                        <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500" >
+                            <img className=' rounded-full' src={week} alt="" />
                         </span>
 
-                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Simple & clean designs</h1>
+                        <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Week Tour</h1>
 
                         <p className="text-gray-500 dark:text-gray-300">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+                            I will go for a week tour to guide you. It will cover all places near cox's bazar , also I will provide you hotel if you want.Let's check it out.
                         </p>
 
-                        <a href="#" className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                            <span className="mx-1">read more</span>
-                            <svg className="w-4 h-4 mx-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                        </a>
+                        <div className='flex justify-between items-center w-full px-4'>
+                            <p className='text-gray-800 font-bold'>Price:</p>
+                            <button className="  -mx-1 text-sm py-2 px-4 rounded-md text-gray-200 bg-blue-500 capitalize transition-colors duration-200 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <span className="mx-1">Checkout</span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
