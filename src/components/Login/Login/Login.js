b8590eb2fee77880b3593e80a54559f154f7471f
Neamul01 <neamulhaque2002@gmail.com>
2022-04-17 21:57:50 +0600

    form validate done

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -5,19 +5,20 @@ import { useForm } from 'react-hook-form'
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const onSubmit = data => {
-        console.log(data)
+    const formSubmit = data => {
+        const { email, password } = data;
+        console.log(email, password)
     }
-
     return (
         <div className="w-full max-w-sm p-6 m-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-24">
             <h1 className="text-3xl font-semibold text-center text-gray-700 dark:text-white">Login</h1>
 
-            <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
+            <form onSubmit={handleSubmit(formSubmit)} className="mt-6">
                 <div>
                     <label htmlFor="username" className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-                    <input {...register("email")} type="email"
+                    <input {...register("email", { required: true })} type="email"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.email && <p><small className='text-red-500'>&diams; This field is Required</small></p>}
                 </div>
 
                 <div className="mt-4">
@@ -26,8 +27,9 @@ const Login = () => {
                         <p className="cursor-pointer text-xs text-gray-600 dark:text-gray-400 hover:underline">Forget Password?</p>
                     </div>
 
-                    <input {...register("password")} type="password"
+                    <input {...register("password", { required: true })} type="password"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.email && <p><small className='text-red-500'>&diams; This Field is Required</small></p>}
                 </div>
 
                 <div className="mt-6">
--- src/components/Login/Signup/Signup.js
+++ src/components/Login/Signup/Signup.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form'
 
 const Signup = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [error, setError] = useState('')
+
+    const formSubmit = data => {
+        const { name, email, password, confirmPassword } = data;
+
+        if (password !== confirmPassword) {
+            setError("Password didn't match")
+        }
+    }
+
     return (
         <div className="w-full max-w-sm p-6 m-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-24">
             <h1 className="text-3xl font-semibold text-center text-gray-700 dark:text-white">Sign Up</h1>
 
-            <form className="mt-6">
+            <form onSubmit={handleSubmit(formSubmit)} className="mt-6">
                 <div>
                     <label htmlFor="username" className="block text-sm text-gray-800 dark:text-gray-200">Name</label>
-                    <input type="text"
+                    <input {...register('name', { required: true })} type="text"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.name && <p><small className="text-red-500">&diams; This Field is Required.</small></p>}
                 </div>
 
                 <div>
-                    <label htmlFor="username" className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-                    <input type="email"
+                    <div className="flex items-center justify-between">
+                        <label htmlFor="password" className="block text-sm mt-4 text-gray-800 dark:text-gray-200">Email</label>
+                    </div>
+                    <input {...register('email', { required: true })} type="email"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.email && <p><small className="text-red-500">&diams; This Field is Required.</small></p>}
                 </div>
 
                 <div className="mt-4">
@@ -24,9 +40,22 @@ const Signup = () => {
                         <label htmlFor="password" className="block text-sm text-gray-800 dark:text-gray-200">Password</label>
                     </div>
 
-                    <input type="password"
+                    <input {...register('password', { required: true })} type="password"
                         className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.password && <p><small className="text-red-500">&diams; This Field is Required.</small></p>}
                 </div>
+
+                <div className="mt-4">
+                    <div className="flex items-center justify-between">
+                        <label htmlFor="confirm-password" className="block text-sm text-gray-800 dark:text-gray-200">Confirm Password</label>
+                    </div>
+
+                    <input {...register('confirmPassword', { required: true })} type="password"
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    {errors.confirmPassword && <p><small className="text-red-500">&diams; This Field is Required.</small></p>}
+                    {error && <p><small className="text-red-500">&diams; This Field is Required.</small></p>}
+                </div>
+
 
                 <div className="mt-6">
                     <button
